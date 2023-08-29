6a86460bd968e3a4aea66efb4727ca90829d9c67
ASPP Student <student@localhost>
2023-08-29 09:56:34 +0300

    initial commit

--- hands_on/local_maxima/local_maxima.py
+++ hands_on/local_maxima/local_maxima.py
@@ -7,4 +7,9 @@ def find_maxima(x):
     Output:
     idx -- list of indices of the local maxima in x
     """
-    return []
+    local_max = []
+    diffs = [f-i for i,f in zip(v[0:-1],v[1:])]
+    for i in range(len(diffs)-1):
+        if diffs[i] > 0 and diffs[i+1] < 0:
+            local_max.append(i+1)
+    return local_max
