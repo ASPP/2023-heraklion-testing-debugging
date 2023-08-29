7a69525cb90fc27bb1a075c221940ef7d934e4b6
ASPP Student <student@localhost>
2023-08-29 10:51:19 +0300

    Implement local_maxima function

--- hands_on/local_maxima_part2/local_maxima.py
+++ hands_on/local_maxima_part2/local_maxima.py
@@ -1,4 +1,4 @@
-def find_maxima(x):
+def find_maxima(points):
     """Find local maxima of x.
 
     Input arguments:
@@ -7,4 +7,19 @@ def find_maxima(x):
     Output:
     idx -- list of indices of the local maxima in x
     """
-    return []
+    indices = []
+    
+    if not points:
+        print("No input given")
+        return []
+    elif len(points) == 1:
+        return [0]
+
+    if points[0] > points[1]:
+        indices.append(0)
+    for i in range(1, len(points)-1):
+        if points[i] > points[i+1] and points[i] > points[i-1]:
+            indices.append(i)
+    if points[-1] > points[-2]:
+        indices.append(len(points)-1)
+    return indices
